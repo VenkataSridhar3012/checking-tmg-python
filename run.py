fcd841d96ae4f2b58777cb5d9b58da7f5f519346
venkatasridhar <pvsridharroyal@gmail.com>
2023-09-28 10:16:10 +0530

    changed database url

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import app, db  # Import the Flask app instance and SQLAlchemy instance
 
-# Create the tables in your database
+#Create the tables in your database
 with app.app_context():
     db.create_all()
 
